ff84ff36f52c635862656b98f1a327fb0a36099e
(Student  C00273450) Oisin Miley <C00273450@setu.ie>
2025-01-16 12:36:06 +0000

    G:/webdev/webdev

---
+++ php1/task1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        $first = 10;
+        $second = 5;
+        $third = 6;
+
+        echo $first + $second + $third;
+         
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
