5f5cdfd2556a227a0a19422e078608612f5e97f2
(Student  C00273450) Oisin Miley <C00273450@setu.ie>
2025-01-16 15:59:36 +0000

    php1

---
+++ php1/task2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        $first = "Hello";
+        $second = "your name";
+
+        echo $first . " " . $second;
+         
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ php1/task3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        $first = 5;
+        $second = 10;
+
+        if($first == $second)
+        {
+            echo $first . " is the same as " . $second
+        }
+        else{
+            echo $first . " and " . $second . " are not the same"
+        }
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
